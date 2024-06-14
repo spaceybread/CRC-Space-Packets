920b67a63fb7c6ac832de3e39b2732fa443cf65e
spaceybread <spaceybread@gmail.com>
2024-06-14 14:07:24 -0500

    samples and CRC check doesn't modify the original file

--- runCRC.py
+++ runCRC.py
@@ -16,16 +16,24 @@ def hash(buf):
     for k in buf:
         crc = (crc >> 8) ^ table[(crc & 0xff) ^ k]
     return crc ^ 0xffffffff
-    
 
 table = table()
-file = open('packet991', 'br+')
+
+os.system('cp packet991 processing')
+
+file = open('processing', 'br+')
+file.seek(-4, os.SEEK_END)
+out = file.read()
+outIn = int.from_bytes(out, byteorder='little')
+file.close()
+
+file = open('processing', 'br+')
 file.seek(-4, os.SEEK_END)
 file.truncate()
 file.close()
 
-file = open('packet991', 'br+')
+file = open('processing', 'br+')
 data = file.read()
 file.close()
 
-print(hex(hash(data)), int(hex(hash(data)), 16) == 1469400536)
+print(hex(hash(data)), int(hex(hash(data)), 16) == 1469400536, outIn)
